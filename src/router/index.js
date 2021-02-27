4d365d0b9fb7e93b18233231b8a4cd1a994bab66
fatihkaan22 <fatihkaan22@gmail.com>
2021-02-27 18:34:48 +0300

    Chart.vue added to route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Chart from '../views/Chart.vue'
 
 Vue.use(VueRouter)
 
@@ -8,7 +9,7 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Chart
   },
   {
     path: '/about',
