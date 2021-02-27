174dd9ec56db9249b790099924532c24eeeb993a
fatihkaan22 <fatihkaan22@gmail.com>
2021-02-27 18:34:27 +0300

    firebase config

--- src/main.js
+++ src/main.js
@@ -2,6 +2,31 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from 'firebase'
+
+require("firebase/firestore");
+
+var firebaseConfig = {
+	apiKey: "AIzaSyDVLyS1JKeeUbB46lSspm3559RYvA_SmFk",
+	authDomain: "progress-44fd9.firebaseapp.com",
+	databaseURL: "https://progress-44fd9-default-rtdb.firebaseio.com",
+	projectId: "progress-44fd9",
+	storageBucket: "progress-44fd9.appspot.com",
+	messagingSenderId: "67525040169",
+	appId: "1:67525040169:web:2b9f610c6e5d99f5256438",
+	measurementId: "G-6WP7M9GWD3"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var db = firebase.firestore();
+
+window.db = db; // to access from anywhere
+
+db.settings({
+	timestampsInSnapshots: true
+});
+
 
 Vue.config.productionTip = false
 
