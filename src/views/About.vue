5134fe83f89513d44501097a4eafa362906589aa
fatihkaan22 <fatihkaan22@gmail.com>
2021-03-01 16:57:20 +0300

    in container

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="about">
+  <div class="about container">
+    <meta name="viewport" content="width=device-width, inital-scale=1.0" />
     <h1>Track your progress and share them!</h1>
   </div>
 </template>
+
+<style>
+
+</style>
