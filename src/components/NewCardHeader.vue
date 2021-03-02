f640a4c78aab3e28d8c4fda725eed904471cf52a
fatihkaan22 <fatihkaan22@gmail.com>
2021-03-02 19:40:08 +0300

    header seperated

---
+++ src/components/NewCardHeader.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="row justify-content-between">
+    <h2>{{ headerText }}</h2>
+    <gb-button
+			v-if="button"
+      :color="getColor()"
+      @click="$emit('click', buttonState)"
+      :left-icon="getIcon()"
+    >
+      {{ getText() }}
+    </gb-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NewCardHeader",
+  props: {
+    headerText: "",
+    buttonState: "", //"add" or "cancel"
+		button: Boolean
+  },
+  methods: {
+    getColor() {
+      if (this.buttonState === "cancel") {
+        return "red";
+      } else {
+        //add
+        return "blue";
+      }
+    },
+    getIcon() {
+      if (this.buttonState === "cancel") {
+        return "close";
+      } else {
+        //add
+        return "add";
+      }
+    },
+    getText() {
+      if (this.buttonState === "cancel") {
+        //this.buttonText = "Cancel"
+        return "Cancel";
+      } else {
+        //add
+        return "New Card";
+      }
+    }
+  }
+};
+</script>
--- src/views/Chart.vue
+++ src/views/Chart.vue
@@ -1,22 +1,26 @@
 <template>
   <div id="app">
     <div class="container">
-      <div class="row justify-content-between">
-        <h2>{{ authUser.displayName }}'s Progress</h2>
-        <gb-button :color="buttonColor" @click="newCard">
-					{{ buttonText }}
-        </gb-button>
-      </div>
+      <NewCardHeader
+        @click="newCardClicked"
+        headerText="Your Progress"
+				:button="true"
+        :buttonState="buttonState"
+      />
       <gb-divider />
       <div class="row justify-content-center mx-auto">
-        <NewProgress v-if="onNewCard" />
+        <NewProgress
+          @value="addNewProgressDone"
+          :card="newCardTemplate"
+          v-if="onNewCard"
+        />
         <div v-for="i in list" class="d-inline">
           <ProgressCard
-            :title="i.name"
-            :identifier="i.identifier"
-            :total="i.total"
-            :currentProgress="i.progress"
-            :colors="i.color"
+						:editable="true"
+            v-bind:key="i.id"
+            @delete="deleteCard"
+            @edit="editCard"
+            :card="i"
           />
         </div>
         <div v-for="index in 2" :key="index" class="hidden d-inline" />
@@ -26,95 +30,106 @@
 </template>
 
 <script>
-import ProgressCard from "@/components/CircularProgress.vue";
 import NewProgress from "@/components/NewProgress.vue";
+import NewCardHeader from "@/components/NewCardHeader.vue";
+import ProgressCard from "@/components/CircularProgress.vue";
 import firebase from "firebase";
-
-var gradients = [
-  ["#2193b0", "#6dd5ed"],
-  ["#cc2b5e", "#753a88"],
-  ["#ee9ca7", "#ffdde1"],
-  ["#de6262", "#ffb88c"],
-  ["#06beb6", "#48b1bf"],
-  ["#eb3349", "#f45c43"],
-  ["#56ab2f", "#a8e063"],
-  ["#02aab0", "#00cdac"],
-  ["#43cea2", "#185a9d"],
-  ["#ff512f", "#dd2476"],
-  ["#4568dc", "#b06ab3"],
-  ["#ec6f66", "#f3a183"],
-  ["#4ca1af", "#c4e0e5"],
-  ["#ff5f6d", "#ffc371"],
-  ["#36d1dc", "#5b86e5"],
-  ["#aa076b", "#61045f"],
-  ["#e96443", "#904e95"],
-  ["#654ea3", "#eaafc8"],
-  ["#8360c3", "#2ebf91"],
-  ["#8E2DE2", "#4A00E0"],
-  ["#7F7FD5", "#91EAE4"],
-  ["#92fe9d", "#00c9ff"],
-  ["#f6d365", "#fda085"]
-];
-
-//var gradients = [
-//];
 
 export default {
   name: "Progress",
   components: {
     ProgressCard,
-    NewProgress
+    NewProgress,
+    NewCardHeader,
   },
   data() {
     return {
       list: [],
-      authUser: {},
+      authUser: { displayName: "Your Progress" },
       onNewCard: false,
-			buttonText: "New Card",
-			buttonColor: "blue"
+      editingCard: {},
+      newCardTemplate: {}
     };
   },
-
   methods: {
-    newCard() {
-			if (this.onNewCard ) {
-				this.buttonText = "New Card";
-				this.buttonColor = "blue";
-			} else {
-				this.buttonText = "Cancel";
-				this.buttonColor = "red";
-			}
+    newCardClicked(state) {
+      //console.log("editingcard", this.editingCard.id);
+      if (this.editingCard.id) {
+        this.addToDB(this.editingCard);
+      }
+      this.newCardTemplate = {};
+      this.changeButtonState();
+    },
+    editCard(card) {
+      this.editingCard = { ...card };
+      this.deleteCard(card.id);
+      this.newCardTemplate = card;
+      this.changeButtonState();
+    },
+    addToDB(item) {
+      if (!this.authUser) return;
+      var ref;
+      if (!item.id) {
+        //console.log("addtodb", item.id);
+        ref = db.ref("users/" + this.authUser.uid + "/list").push();
+      } else {
+        ref = db.ref("users/" + this.authUser.uid + "/list/" + item.id);
+      }
+      item.id = null;
+      ref
+        .set(item)
+        .then(() => {
+          //console.log("Success");
+        })
+        .catch(() => {
+          console.error("Error");
+        });
+    },
+    deleteCard(id) {
+      var cardRef = db.ref("users/" + this.authUser.uid + "/list/" + id);
+      cardRef
+        .set({})
+        .then(() => {
+          //console.log("Success");
+        })
+        .catch(() => {
+          console.error("Error");
+        });
+
+      //console.log(id);
+    },
+    addNewProgressDone(item) {
+      this.addToDB(item);
+      this.changeButtonState();
+    },
+    changeButtonState() {
+      if (this.onNewCard) {
+        this.buttonState = "add";
+      } else {
+        this.buttonState = "cancel";
+      }
       this.onNewCard = !this.onNewCard;
     },
-    progress(event, progress, stepValue) {
-      //console.log(stepValue);
-    },
-    progress_end(event) {
-      //console.log("Circle progress end");
-    },
     fetchData() {
-      console.log("here" + this.authUser.uid);
       var listRef = db.ref("users/" + this.authUser.uid + "/list");
       listRef.on("value", snapshot => {
         let list = [];
         let i = 0;
         snapshot.forEach(function(childSnapshot) {
-          //          for (var i = 0, len = gradients.length; i < len; i++) {
-          // FIXME: debug
           var item = childSnapshot.val();
-          item.color = gradients[i++ % gradients.length];
+          item.id = childSnapshot.key;
           list.push(item);
-          //         }
         });
-        this.list = list;
+        this.list = list.reverse();
       });
     }
   },
   created() {
+    this.buttonState = "add";
     firebase.auth().onAuthStateChanged(user => {
       if (user) {
         this.authUser = user;
-        console.log(user.email); // FIXME
+        //console.log(user.email); // FIXME
         this.fetchData();
       } else {
         //TODO
@@ -140,7 +155,6 @@ export default {
 h2 {
   color: white;
 }
-
 .hidden {
   width: 19rem;
   margin: 1rem;
