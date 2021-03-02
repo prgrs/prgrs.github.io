ae1fa5cd02b748444785935e9407f731259e1585
fatihkaan22 <fatihkaan22@gmail.com>
2021-03-02 19:38:34 +0300

    about name changed to share

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
+      <router-link to="/about">Share</router-link> |
       <router-link to="/login">Login</router-link>
     </div>
     <router-view/>
@@ -19,15 +19,16 @@
 }
 
 #nav {
+	font-size: 18px;
   padding: 30px;
 }
 
 #nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: #a9c7df;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: #26c1c9;
 }
 </style>
